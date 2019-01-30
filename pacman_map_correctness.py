3f95fac38b707fa3911d4560548375345768f904
zahra <zahra_9877@yahoo.com>
2019-01-30 05:14:50 +0330

    Add number_fo_super_food condition

--- pacman_map_correctness.py
+++ pacman_map_correctness.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
+
 # python imports
 import sys
 import json
 
 
-def is_map_correct(my_map):
-    ghosts = my_map['players']['ghosts']
-    board = my_map['board']
+def calculate_super_food (board):
+
+    num_of_super_food = 0
+    for cell in board:
+        if cell.includes("s"):
+            num_of_super_food += 1
+
+    return num_of_super_food
+
+
+def is_map_correct(map_config):
+    ghosts = map_config['players']['ghosts']
+    board = map_config['board']
     ghost_positions = []
     is_correct = True
+    board_width = len(board[0])
+    board_height = len(board)
+    total_seeds = board_width * board_height
+    num_of_super_food = calculate_super_food(board)
+
+    # check ghost positions
     for ghost in ghosts:
         ghost_positions.append(ghost['position'])
 
@@ -17,9 +34,13 @@ def is_map_correct(my_map):
             print('Invalid ghost position')
             is_correct = False
             break
+    # check number_of_super_food
+    if num_of_super_food/total_seeds > .025:
+        print("Number of super foods are out of range!!!")
+        is_correct = False
+
     return is_correct
     
-
 
 def load_file(map_path):
     with open((map_path), "r") as map_file:
